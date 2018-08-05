22cc02bbf8c6e62d11caf1697be99719dde92411
CaladoLucas <lcaraujo4252@gmail.com>
2018-08-04 21:28:05 -0300

    first

---
+++ app/src/main/java/com/example/lucas/pokemonwiki/BulbassaurActivity.kt
@@ -0,0 +1,12 @@
+package com.example.lucas.pokemonwiki
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class BulbassaurActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bulbassaur)
+    }
+}
